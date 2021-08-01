2c9b239c67fc8fd151b47570b02bd4bbc97060a9
Gerson Azabache <gerson_dunk@hotmail.com>
2021-08-01 15:45:24 -0500

    creating post method

---
+++ Entities/Entities/Contrato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Entities
+{
+    public class Contrato
+    {
+        public int Id { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public bool IsCombustible { get; set; }
+        public bool IsTransporte { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
--- Mach/Shared/ApiHelper.cs
+++ Mach/Shared/ApiHelper.cs
@@ -1,11 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Mach.Shared
 {
@@ -45,6 +42,23 @@ namespace Mach.Shared
             }
             return string.Empty;
         }
+
+        //public static async Task<string> Post(Entities id)
+        //{
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        using (HttpResponseMessage res = await client.GetAsync(baseURL + $"/{id}"))
+        //        {
+        //            using (HttpContent content = res.Content)
+        //            {
+        //                string data = await content.ReadAsStringAsync();
+        //                if (data != null)
+        //                    return data;
+        //            }
+        //        }
+        //    }
+        //    return string.Empty;
+        //}
 
 
         public static string BeautifyJson(string jsonString)
